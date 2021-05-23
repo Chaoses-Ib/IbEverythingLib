c4f7aee869cac1355e82c91c434c2f41fa6e7fda
Chaoses Ib <Chaos-es@outlook.com>
2021-05-24 01:17:09 +0800

    Improve Test.cpp

--- Cpp/Test.cpp
+++ Cpp/Test.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <string>
 #include "Everything.hpp"
 
@@ -16,11 +17,13 @@ int main()
         ev.query_send(LR"(infolder:"C:\Program Files (x86)\")", 0, Request::FileName | Request::Size);
         QueryResults results = ev.query_get();  //or query_future().get()
         DWORD num = results.query_num;
-        cout << num << " ";
+        wcout << num << endl;
         for (DWORD i = 0; i < num; i++) {
             wstring s = results[i].get_str(Request::FileName);
             uint64_t size = results[i].get_size();
+            if constexpr (debug)  //Everythings::debug
+                wcout << left << setw(30) << s << (size >> 30) << L"GB" << endl;
         }
     }
-    cout << timeGetTime() - t << "ms" << endl;
+    wcout << timeGetTime() - t << L"ms" << endl;
 }

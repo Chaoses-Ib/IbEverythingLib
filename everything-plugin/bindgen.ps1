672582c238a08b946e173134d6c43080d82f1afb
Chaoses-Ib <Chaos-es@outlook.com>
2025-06-11 17:55:01 +0800

    fix(plugin/sys): override abi

--- everything-plugin/bindgen.ps1
+++ everything-plugin/bindgen.ps1
@@ -1,4 +1,11 @@
 ﻿uv run ./download_sdk.py
 
 # TODO: https://github.com/rust-lang/rust-bindgen/issues/2535
-bindgen ../target/sdk/everything_plugin.h -o src/sys.rs --allowlist-file '../target/sdk/everything_plugin.h' --raw-line '#![allow(non_snake_case, non_camel_case_types, dead_code)]' --merge-extern-blocks --rustified-enum .* --verbose
+bindgen ../target/sdk/everything_plugin.h -o src/sys.rs `
+    --allowlist-file '../target/sdk/everything_plugin.h' `
+    --override-abi '.*=system' `
+    --raw-line '//! Reference: https://www.voidtools.com/forum/viewtopic.php?t=16535' `
+    --raw-line '#![allow(non_snake_case, non_camel_case_types, dead_code)]' `
+    --merge-extern-blocks `
+    --rustified-enum .* `
+    --verbose

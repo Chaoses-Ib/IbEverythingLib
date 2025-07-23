38eaa0508023b3f381aa9d94622e461b6b9ef332
Chaoses-Ib <Chaos-es@outlook.com>
2025-07-23 18:55:22 +0800

    feat(plugin/log): `tracing-appender` feature

--- everything-plugin/src/log.rs
+++ everything-plugin/src/log.rs
@@ -4,9 +4,17 @@ pub use tracing::{debug, error, info, trace, warn};
 /// A convenient function to initialize [`tracing`] with a default configuration.
 #[cfg(feature = "tracing")]
 pub fn tracing_init() {
+    #[cfg(not(feature = "tracing-appender"))]
+    let stderr = anstream::stderr;
+    #[cfg(feature = "tracing-appender")]
+    let stderr = {
+        let (non_blocking, guard) = tracing_appender::non_blocking(anstream::stderr());
+        std::mem::forget(guard);
+        non_blocking
+    };
+
     tracing_subscriber::fmt()
-        // TODO: Non-block?
-        .with_writer(anstream::stderr)
+        .with_writer(stderr)
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
